807250f6addbb1673cf7127fb99635074260ccbb
Kevin Gilpin <kgilpin@gmail.com>
2013-02-03 11:23:45 -0500

    use the correct table name

--- lib/skip32/activerecord.rb
+++ lib/skip32/activerecord.rb
@@ -18,7 +18,7 @@ module Skip32
     def acts_as_skip32(table, column)
       change_column table.to_sym, column.to_sym, :text
       execute <<-SQL
-        ALTER TABLE "products"
+        ALTER TABLE "#{table.to_s}"
           ALTER COLUMN "#{column.to_s}"
           set DEFAULT crockford(skip32('#{table.to_s}'::text, nextval('#{table.to_s}_id_seq'::regclass)))
       SQL
